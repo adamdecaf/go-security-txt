5c314c2ff2f2c4e4069586c9ef840fdcbb32f4c0
Adam Shannon <adamkshannon@gmail.com>
2017-10-11 21:36:03 -0500

    cmd: basic

---
+++ cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	sectext "github.com/adamdecaf/go-security-txt"
+)
+
+var (
+	fs = flag.NewFlagSet("flag", flag.ExitOnError)
+
+	address = fs.String("address", "", "The address to check for a security.txt file")
+)
+
+func main() {
+	fs.Parse(os.Args[1:])
+
+	if address != nil && *address != "" {
+		sec, err := sectext.Read(*address)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		output(*address, *sec)
+		os.Exit(0)
+	}
+
+	fs.PrintDefaults()
+	os.Exit(1)
+}
+
+func output(addr string, sec sectext.SecurityTxt) {
+	fmt.Printf("security.txt for %s\n", addr)
+	fmt.Printf("  Contact: %s\n", sec.Contact)
+	if !sec.Acknowledgements.Empty() {
+		fmt.Printf("  Acknowledgements: %s\n", sec.Acknowledgements)
+	}
+	if !sec.Disclosure.Empty() {
+		fmt.Printf("  Disclosure: %s", sec.Disclosure)
+	}
+	if !sec.Encryption.Empty() {
+		fmt.Printf("  Encryption: %s", sec.Encryption)
+	}
+}
